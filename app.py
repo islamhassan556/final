f8669c7e91f1714ebaee674b1f87be6359b5a205
islamhassan556 <144813743+islamhassan556@users.noreply.github.com>
2024-05-05 22:29:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -94,20 +94,20 @@ def predict():
     score = predicted_proba[np.argmax(predicted_proba)]
 
     if score > 0.30:
-    # Translate predicted disease to the input language
-    if lang == 'ar':
-        translated_disease = translate_to_arabic(predicted_disease)
-        response = {'predicted': "من المحتمل أنك تعاني من " + translated_disease}
-    else:
-        response = {'predicted': f"Maybe you suffer from {predicted_disease}"}
+        # Translate predicted disease to the input language
+        if lang == 'ar':
+            translated_disease = translate_to_arabic(predicted_disease)
+            response = {'predicted': "من المحتمل أنك تعاني من " + translated_disease}
+        else:
+            response = {'predicted': f"Maybe you suffer from {predicted_disease}"}
     
-    return jsonify(response), 200
-else:
-    if lang == 'ar':
-        response = {'error': 'من فضلك ادخل أعراض صحيحة'}
-    else:
-        response = {'error': 'Please enter valid symptoms'}
+        return jsonify(response), 200
+   else:
+       if lang == 'ar':
+           response = {'error': 'من فضلك ادخل أعراض صحيحة'}
+       else:
+           response = {'error': 'Please enter valid symptoms'}
     
-    return jsonify(response), 400
+       return jsonify(response), 400
 if __name__ == '__main__':
     app.run(debug=True)
